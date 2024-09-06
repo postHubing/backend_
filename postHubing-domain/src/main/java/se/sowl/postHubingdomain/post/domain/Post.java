6bf9190ea3735d8a39cca8cdb1ccf8ac403bd8ce
강철웅 <kcw130502@gmail.com>
2024-09-06 14:52:11 +0900

    feat: Post, User 엔터티 추가

--- postHubing-domain/src/main/java/se/sowl/postHubingdomain/post/domain/Post.java
+++ postHubing-domain/src/main/java/se/sowl/postHubingdomain/post/domain/Post.java
@@ -1,4 +1,46 @@
 package se.sowl.postHubingdomain.post.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import se.sowl.postHubingdomain.user.domain.User;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Getter
+@Table(name = "posts")
 public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Column(nullable = false)
+    private String category;
+
+
+
+
+
+
+
+
+
 }
--- postHubing-domain/src/main/java/se/sowl/postHubingdomain/user/domain/User.java
+++ postHubing-domain/src/main/java/se/sowl/postHubingdomain/user/domain/User.java
@@ -7,33 +7,39 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import se.sowl.postHubingdomain.post.domain.Post;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // setter
+@Getter // getter
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //primary키(기본키) 자동생성
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false)  //필수값
     private String name;
 
     private String nickname;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false) //unique=true는 유일한 값
     private String email;
 
     @Column(nullable = false)
     private String provider;
 
-    @CreationTimestamp
+    @CreationTimestamp //현재시간 알려줌
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Post> posts = new ArrayList<>();
 
     @Builder
     public User(Long id, String name, String nickname, String email, String provider) {
