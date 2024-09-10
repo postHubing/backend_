23353be6519e5511c76bf0089c2ec12a6f78402e
kangcheolung <112637112+kangcheolung@users.noreply.github.com>
2024-09-10 23:15:09 +0900

    feat: PostServiceTest 생성

---
+++ postHubing-api/src/test/java/se/sowl/postHubingapi/post/service/PostServiceTest.java
@@ -0,0 +1,22 @@
+package se.sowl.postHubingapi.post.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import se.sowl.postHubingdomain.post.repository.PostRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class PostServiceTest {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private PostService postService;
+
+
+
+}
