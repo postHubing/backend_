5f71b672e2aad74542c1b334b7644156b31af57a
kangcheolung <skhu202114111@gmail.com>
2024-09-11 10:56:25 +0900

    change: PostServiceTest 수정

--- postHubing-api/src/test/java/se/sowl/postHubingapi/post/service/PostServiceTest.java
+++ postHubing-api/src/test/java/se/sowl/postHubingapi/post/service/PostServiceTest.java
@@ -3,6 +3,7 @@ package se.sowl.postHubingapi.post.service;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import se.sowl.postHubingdomain.post.domain.Post;
 import se.sowl.postHubingdomain.post.repository.PostRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,6 +17,10 @@ class PostServiceTest {
 
     @Autowired
     private PostService postService;
+
+    private Post postTest;
+
+
 
 
 
