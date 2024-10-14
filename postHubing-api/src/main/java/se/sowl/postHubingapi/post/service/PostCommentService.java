a7e7b80625794e2c8a624e888b78697a859d7044
kangcheolung <skhu202114111@gmail.com>
2024-10-14 23:45:10 +0900

    add: PostCommentService 추가

--- postHubing-api/src/main/java/se/sowl/postHubingapi/post/service/PostCommentService.java
+++ postHubing-api/src/main/java/se/sowl/postHubingapi/post/service/PostCommentService.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import se.sowl.postHubingapi.post.exception.PostException;
+import se.sowl.postHubingapi.post.exception.UserException;
 import se.sowl.postHubingapi.response.PostCommentResponse;
 import se.sowl.postHubingdomain.post.domain.Post;
 import se.sowl.postHubingdomain.post.domain.PostComment;
 import se.sowl.postHubingdomain.post.repository.PostCommentRepository;
 import se.sowl.postHubingdomain.post.repository.PostRepository;
+import se.sowl.postHubingdomain.user.domain.User;
+import se.sowl.postHubingdomain.user.repository.UserRepository;
 
 import java.util.List;
 
@@ -20,6 +23,7 @@ public class PostCommentService {
     private final PostCommentRepository postCommentRepository;
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public List<PostCommentResponse> getCommentsByPostId(Long postId) {
@@ -32,6 +36,27 @@ public class PostCommentService {
         return comments.stream()
                 .map(PostCommentResponse::from)
                 .toList();
+    }
+
+    @Transactional
+    public PostCommentResponse createComment(Long postId, String content, Long userId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(PostException.PostNotFoundException::new);
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(UserException.UserNotFoundException::new);
+
+        PostComment postComment = PostComment.builder()
+                .post(post)
+                .user(user)
+                .content(content)
+                .build();
+
+        postCommentRepository.save(postComment);
+
+        return PostCommentResponse.from(postComment);
+
+
     }
 
 }
